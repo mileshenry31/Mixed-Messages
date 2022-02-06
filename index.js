b0fd5437819f09886b0e992c0bfddfdc4ec9f448
mileshenry31 <73675082+mileshenry31@users.noreply.github.com>
2022-02-05 22:00:42 -0800

    Added the different types of words

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let who = [];
+let who = ["You", "Your friend", "Your mom", "Your dad", "Your sibling"];
 let good = true;
-let type = [];
-let time = [];
+let what = ["test", "day", "party", "game"]
+let time = ["today", "tomorrow", "yesterday", "soon", "never"];
