9270d3afb1c8b32ae0c83b974f485fdc5c709646
mileshenry31 <73675082+mileshenry31@users.noreply.github.com>
2022-02-05 22:11:36 -0800

    Finished the app

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 let who = ["You", "Your friend", "Your mom", "Your dad", "Your sibling"];
-let good = true;
+let good = ["good", "bad"];
 let what = ["test", "day", "party", "game"]
 let time = ["today", "tomorrow", "yesterday", "soon", "never"];
+
+function makeMessage() {
+    let thePerson = who[Math.floor(Math.random() * who.length)];
+    let mood = good[Math.floor(Math.random() * good.length)];
+    let verb = "";
+    if (thePerson == "You") {
+        verb = "are";
+    } else {
+        verb = "is";
+    }
+    console.log(`${thePerson} ${verb} ${mood}`);
+}
+
+makeMessage();
